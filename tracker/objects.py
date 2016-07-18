67efc983898134620915c992b1776b14765fb55a
Zhang Yiwen <yiwen@pigeonlab.com>
2016-07-18 11:22:48 +0800

    follow google python standards

--- tracker/objects.py
+++ tracker/objects.py
@@ -21,15 +21,15 @@ class Position3D:
 
 class Camera:
 	'class for camera'
-	cameraCount = 0
+	_camera_count = 0
 	def __init__(self, parameters):
 		self.rot = parameters.rot or (0, 0, 0, 0, 0, 0, 0, 0)
 		self.trans = parameters.trans or Position3D(0, 0, 0)
-		self.id = Camera.cameraCount
+		self.id = Camera._camera_count
 		self.focal = parameters.focal or 1720
 		self.k1 = parameters.k1 or 0
 		self.k2 = parameters.k2 or 0
-		Camera.cameraCount += 1
+		Camera._camera_count += 1
 	
 	def __str__(self):
 		return "%s %s %s\n%s %s %s\n%s %s %s\n%s %s %s\n%s %s %s\n%s\n" % self.focal, self.k1, self.k2, self.rot[0], self.rot[1], self.rot[2], self.rot[3], self.rot[4], self.rot[5], self.rot[6], self.rot[7], self.rot[8], str(self.trans)
@@ -37,93 +37,94 @@ class Camera:
 
 class Observation:
 	'class for feature observation in one camera view'
-	observationCount = 0
+	_obs_count = 0
 	def __init__(self, parameters):
-		self.id = Observation.obsCount
+		self.id = Observation._obs_count
 		self.position = parameters.position or Position2D(0, 0)
 		self.color = parameters.color or (0, 0, 0)
 		self.camera = parameters.camera
-		Observation.obsCount += 1
+		Observation._obs_count += 1
 	
 	def __str__(self):
 		return "%s %s %s" % self.camera.id, self.id, str(self.position)
 
 class Point:
 	'class for an object point in 3D space'
-	pointCount = 0
+	_point_count = 0
 	def __init__(self, parameters):
-		self.id = Point.pointCount
+		self.id = Point._point_count
 		self.position = parameters.position or Position3D(0, 0, 0)
 		self.color = parameters.color
-		self.obsList = parameters.obsList or []
+		self.obs_list = parameters.obs_list or []
+		Point._point_count += 1
 
-	def printObsList(self):
-		printString = ""
-		for obs in obsList:
-			printString = printString + str(obs) + " "
-		return printString
+	def print_obs_list(self):
+		print_string = ""
+		for obs in obs_list:
+			print_string = print_string + str(obs) + " "
+		return print_string
 	
 	def __str__(self):
-		return "%s\n%s %s %s\n%s\n" % str(self.position), self.color[0], self.color[1], self.color[2], printObsList
+		return "%s\n%s %s %s\n%s\n" % str(self.position), self.color[0], self.color[1], self.color[2], print_obs_list()
 	
 
 class Tracker:
-	def __init__(self, imageDir):
-		self.featureParams = dict(  maxCorners = 5000,
+	def __init__(self, image_dir):
+		self.feature_params = dict(  maxCorners = 5000,
 									qualityLevel = 0.02,
 									minDistance = 40,
 									blockSize = 20,
 									mask = None,
 									useHarrisDetector=True)
-		self.lucasPramas = dict(	winSize  = (20,20),
+		self.lucas_params = dict(	winSize  = (20,20),
                  					maxLevel = 8,
                   					criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 0.3))
-		self.imageDir = imageDir
-		self.imagePaths = None
+		self.image_dir = image_dir
+		self.image_paths = None
 		self.corners = None
-		self.referenceImageGray = None
+		self.reference_image_gray = None
 
-	def getCorners(self):
+	def get_corners(self):
 		# get absolute path of the first image
-		self.imagePaths = [os.path.join(self.imageDir,fn) for fn in next(os.walk(self.imageDir))[2]]
-		referenceImagePath = self.imagePaths[0]
+		self.image_paths = [os.path.join(self.image_dir,fn) for fn in next(os.walk(self.image_dir))[2]]
+		reference_image_path = self.image_paths[0]
 
 		# find corner in the first image
-		referenceImage = cv2.imread(referenceImagePath)
-		referenceImageGray = cv2.cvtColor(referenceImage, cv2.COLOR_BGR2GRAY)
-		self.referenceImageGray = referenceImageGray
-		self.corners = cv2.goodFeaturesToTrack(referenceImageGray, **self.featureParams)
+		reference_image = cv2.imread(reference_image_path)
+		reference_image_gray = cv2.cvtColor(reference_image, cv2.COLOR_BGR2GRAY)
+		self.reference_image_gray = reference_image_gray
+		self.corners = cv2.goodFeaturesToTrack(reference_image_gray, **self.feature_params)
 	
-	def trackCorners(self):
-		trackImagePaths = self.imagePaths[1:]
-		referenceCorners = self.corners
+	def track_corners(self):
+		track_image_corners = self.image_paths[1:]
+		reference_corners = self.corners
 		# track corners on each image
-		for imagePath in trackImagePaths:
+		for image_path in track_image_corners:
 			# load tracking image
-			currentImage = cv2.imread(imagePath)
-			currentImageGray = cv2.cvtColor(currentImage, cv2.COLOR_BGR2GRAY)
+			current_image = cv2.imread(image_path)
+			current_image_gray = cv2.cvtColor(current_image, cv2.COLOR_BGR2GRAY)
 			# calculate optical flow
-			trackedCorners, status, err = cv2.calcOpticalFlowPyrLK(referenceImageGray, currentImageGray, referenceCorners, None, **self.lucasPramas)
-			goodTrackedCorners = trackedCorners[status==1]
-			# goodReferenceCorners = referenceCorners[status==1]
-			referenceCorners = goodTrackedCorners.reshape(-1, 1, 2)
-		self.corners = referenceCorners
+			tracked_corners, status, err = cv2.calcOpticalFlowPyrLK(referenceImageGray, currentImageGray, reference_corners, None, **self.lucas_params)
+			good_tracked_corners = tracked_corners[status==1]
+			# good_reference_corners = reference_corners[status==1]
+			reference_corners = good_tracked_corners.reshape(-1, 1, 2)
+		self.corners = reference_corners
 
 
 class Problem:
-	def __init__(self, imageDir):
-		self.imageDir = imageDir
+	def __init__(self, image_dir):
+		self.image_dir = image_dir
 		self.cam = []
 		self.obs = []
 		self.pts = []
 	
 	def track(self):
-		cornerFilter = dict( edge = True,
-							 homography = True)
-		tracker = Tracker(self.imageDir)
-		tracker.getCorners()
-		# tracker.trackCorners()
-		# tracker.filterCorners(cornerFilter, )
+		corner_filter = dict(edge = True,
+							homography = True)
+		tracker = Tracker(self.image_dir)
+		tracker.get_corners()
+		# tracker.track_corners()
+		# tracker.filter_corners(cornerFilter, )
 		
 		
 
