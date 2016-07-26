0982fe48e73f326a0110512a3b2ba59deb759436
Zhang Yiwen <yiwen@pigeonlab.com>
2016-07-26 17:44:13 +0800

    modify to bundler 3

--- ceres-solver-1.11.0/examples/bal_problem_backup.cc
+++ ceres-solver-1.11.0/examples/bal_problem_backup.cc
@@ -39,7 +39,6 @@
 #include "ceres/rotation.h"
 #include "glog/logging.h"
 #include "random.h"
-#include <iostream>
 
 namespace ceres {
 namespace examples {
@@ -50,7 +49,6 @@ typedef Eigen::Map<const Eigen::VectorXd> ConstVectorRef;
 template<typename T>
 void FscanfOrDie(FILE* fptr, const char* format, T* value) {
   int num_scanned = fscanf(fptr, format, value);
-  std::cout << "hey hey hey " << *value;
   if (num_scanned != 1) {
     LOG(FATAL) << "Invalid UW data file.";
   }
@@ -82,9 +80,7 @@ BALProblem::BALProblem(const std::string& filename, bool use_quaternions) {
   // This wil die horribly on invalid files. Them's the breaks.
   FscanfOrDie(fptr, "%d", &num_cameras_);
   FscanfOrDie(fptr, "%d", &num_points_);
-  num_observations_ = num_cameras_ * num_points_;
-  std::cout << "observation: " << num_observations_ << std::endl;
-  // FscanfOrDie(fptr, "%d", &num_observations_);
+  FscanfOrDie(fptr, "%d", &num_observations_);
 
   VLOG(1) << "Header: " << num_cameras_
           << " " << num_points_
@@ -93,7 +89,6 @@ BALProblem::BALProblem(const std::string& filename, bool use_quaternions) {
   point_index_ = new int[num_observations_];
   camera_index_ = new int[num_observations_];
   observations_ = new double[2 * num_observations_];
-  colors_ = new int[3 * num_points_]
 
   num_parameters_ = 9 * num_cameras_ + 3 * num_points_;
   parameters_ = new double[num_parameters_];
@@ -112,29 +107,29 @@ BALProblem::BALProblem(const std::string& filename, bool use_quaternions) {
 
   fclose(fptr);
 
-  // use_quaternions_ = use_quaternions;
-  // if (use_quaternions) {
-  //   // Switch the angle-axis rotations to quaternions.
-  //   num_parameters_ = 10 * num_cameras_ + 3 * num_points_;
-  //   double* quaternion_parameters = new double[num_parameters_];
-  //   double* original_cursor = parameters_;
-  //   double* quaternion_cursor = quaternion_parameters;
-  //   for (int i = 0; i < num_cameras_; ++i) {
-  //     AngleAxisToQuaternion(original_cursor, quaternion_cursor);
-  //     quaternion_cursor += 4;
-  //     original_cursor += 3;
-  //     for (int j = 4; j < 10; ++j) {
-  //      *quaternion_cursor++ = *original_cursor++;
-  //     }
-  //   }
-  //   // Copy the rest of the points.
-  //   for (int i = 0; i < 3 * num_points_; ++i) {
-  //     *quaternion_cursor++ = *original_cursor++;
-  //   }
-  //   // Swap in the quaternion parameters.
-  //   delete []parameters_;
-  //   parameters_ = quaternion_parameters;
-  // }
+  use_quaternions_ = use_quaternions;
+  if (use_quaternions) {
+    // Switch the angle-axis rotations to quaternions.
+    num_parameters_ = 10 * num_cameras_ + 3 * num_points_;
+    double* quaternion_parameters = new double[num_parameters_];
+    double* original_cursor = parameters_;
+    double* quaternion_cursor = quaternion_parameters;
+    for (int i = 0; i < num_cameras_; ++i) {
+      AngleAxisToQuaternion(original_cursor, quaternion_cursor);
+      quaternion_cursor += 4;
+      original_cursor += 3;
+      for (int j = 4; j < 10; ++j) {
+       *quaternion_cursor++ = *original_cursor++;
+      }
+    }
+    // Copy the rest of the points.
+    for (int i = 0; i < 3 * num_points_; ++i) {
+      *quaternion_cursor++ = *original_cursor++;
+    }
+    // Swap in the quaternion parameters.
+    delete []parameters_;
+    parameters_ = quaternion_parameters;
+  }
 }
 
 // This function writes the problem to a file in the same format that
