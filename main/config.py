2800f12ebbfaa10b3a8a4f0de20e8547a6ef7a0b
Zhang Yiwen <yiwen@pigeonlab.com>
2016-07-30 23:24:18 +0800

    update config

--- main/config.py
+++ main/config.py
@@ -1,19 +1,19 @@
 import cv2
 
-FEATURE_PARAMS 		= dict( maxCorners        = 5000,
+FEATURE_PARAMS 		= dict(	maxCorners        = 5000,
 							qualityLevel      = 0.03,
 		 					minDistance       = 40,
 							blockSize         = 20,
 							mask              = None,
 							useHarrisDetector = True)
 
-LOCAS_PARAMS 		= dict( winSize  = (25,25),
+LOCAS_PARAMS 		= dict(	winSize  = (25,25),
                  			maxLevel = 8,
             				criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 20, 0.3))
 
 HOMOGRAPHY_PARAMS 	= dict(	ransacThreshold = 0.05)
 
-CAMERA_PARAMS		= dict( imageWidth  = 3264,
+CAMERA_PARAMS		= dict(	imageWidth  = 3264,
 							imageHeight = 2448,
 							focal       = 2720.0,
 							k1          = 0,
@@ -21,8 +21,8 @@ CAMERA_PARAMS		= dict( imageWidth  = 3264,
 							rot         = (1, 0, 0, 0, 1, 0, 0, 0, 1),
 							estDistance = 700)
 
-CORNER_FILTER		= dict( edge       = False,
+CORNER_FILTER		= dict(	edge       = False,
 							homography = True)
 
-CERES_PARAMS        = dict( solverPath    = '../ceres-bin/bin/bundle_adjuster',
+CERES_PARAMS        = dict(	solverPath    = '../ceres-bin/bin/bundle_adjuster',
 							maxIterations = 100)
